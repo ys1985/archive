c61eed94e075c723d66f1760e392db6d370f6290
YutaShimizu <s.yuta02@gmail.com>
2017-09-28 18:55:38 +0900

    test

---
+++ react-material-ui/containers/App.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import MainSection from '../components/MainSection';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <MainSection />
+      </div>
+    );
+  }
+}
+
+export default App;
--- react-material-ui/webpack.config.js
+++ react-material-ui/webpack.config.js
@@ -1,12 +1,30 @@
 module.exports = {
-  entry: "./entry.js",
+  context: __dirname + "/src",
+  entry: {
+    jsx: "./index.jsx",
+    css: "./main.css",
+    html: "./index.html",
+  },
+
   output: {
-    path: __dirname,
-    filename: "bundle.js"
+    path: __dirname + "/static",
+    filename: "bundle.js",
   },
   module: {
+    preLoaders: [
+        //Eslint loader
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "eslint-loader"},
+    ],
     loaders: [
-      { test: /\.js$/,  loader: "babel-loader", exclude: /node_modules/ }
-    ]
-  }
-}
+      { test: /\.html$/, loader: "file?name=[name].[ext]" },
+      { test: /\.css$/, loader: "file?name=[name].[ext]" },
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader?stage=0&optional=runtime"]},
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  // eslint: {
+  //   configFile: './.eslintrc'
+  // },
+};
