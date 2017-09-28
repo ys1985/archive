20e26359a927f59184326a3078b7aa3eebb7c267
YutaShimizu <s.yuta02@gmail.com>
2017-09-28 16:57:55 +0900

    test

--- react-material-ui/entry.js
+++ react-material-ui/entry.js
@@ -3,8 +3,14 @@ var ReactDOM = require('react-dom');
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
+import CircularProgress from 'material-ui/CircularProgress';
 
 ReactDOM.render(
+ //  <div>
+ //   <CircularProgress />
+ //   <CircularProgress size={60} thickness={7} />
+ //   <CircularProgress size={80} thickness={5} />
+ // </div>,
   <div>
     <h1>Hello, world</h1>
     <MuiThemeProvider>
